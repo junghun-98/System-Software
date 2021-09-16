bb3f98a294d89b3eff3afbad1219bef914e1bd61
junghun-98 <cjswpwjdgns@naver.com>
2021-09-16 17:15:56 +0900

    HW third week

---
+++ ex_mkdir.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define PERMS 0755
+#define MAX_PATH_LEN 1024
+
+int main(int argc, char const * argv[]){
+	if(argc != 2){
+		printf("Usage %s [pathname]\n", argv[0]);
+		exit(-1);
+	}
+
+	if(mkdir(argv[1], PERMS) == -1){
+		perror("mkdir() error!");
+		exit(-2);
+	}	
+	
+	return 0;
+}
---
+++ ex_readdir.c
@@ -0,0 +1,36 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char const * argv[]){
+	struct stat fileInfo;
+
+	DIR * dirp;
+	struct dirent * dirInfo;
+
+	if(argc != 2){
+		printf("Usage: %s [pathname]\n", argv[0]);
+		exit(-1);
+	}
+
+	if(stat(argv[1], &fileInfo) == -1){
+		perror("stat() error!");
+		exit(-1);
+	}
+
+	if(!S_ISDIR(fileInfo.st_mode)){
+		fprintf(stderr, "%s is not directory!\n", argv[1]);
+		exit(-1);
+	}
+
+	dirp = opendir(argv[1]);
+	while((dirInfo = readdir(dirp)) != NULL){
+		printf("inode No.: %ld, Name: %s\n", dirInfo->d_ino, dirInfo->d_name);
+	}
+
+	closedir(dirp);
+
+	return 0;
+}
